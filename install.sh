a34a582751492aa163bf72bed2135f2a69ade66e
Matthias Budde <matt@nordseele.com>
2020-12-25 13:23:24 +0000

    init

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@ sudo apt update -y
 
 sudo SKIP_WARNING=1 rpi-update
 
-
-
-sudo cp /home/pi/hans_install/hans.service /etc/systemd/system
+sudo cp /home/pi/hans_install/files/hans.service /etc/systemd/system
 
 sudo systemctl enable hans
 
@@ -20,12 +18,12 @@ cd
 
 wget https://github.com/antiprism/amidiauto/releases/download/v1.01Raspbian/amidiauto-1.01_buster.deb
 sudo apt install ./amidiauto-1.01_buster.deb
-sudo cp /home/pi/hans_install/amidiauto.conf /etc/amidiauto.conf
+sudo cp /home/pi/hans_install/files/amidiauto.conf /etc/amidiauto.conf
 sudo systemctl enable amidiauto
 
-sudo cp /home/pi/hans_install/mod-ttymidi.service /etc/systemd/system/mod-ttymidi.service
+sudo cp /home/pi/hans_install/files/mod-ttymidi.service /etc/systemd/system/mod-ttymidi.service
 sudo systemctl enable mod-ttymidi.service
-sudo cp /home/pi/hans_install/cmdline.txt /boot
+sudo cp /home/pi/hans_install/files/cmdline.txt /boot
 
 
 #I2C
@@ -36,18 +34,18 @@ sudo sh -c "echo 'dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=2,i2c_gpio_sda=5,i2
 sudo raspi-config nonint do_expand_rootfs
 
 
-# WIFI 
+# NETWORK ? :-/
 sudo apt-get install comitup -y
 sudo systemctl disable systemd-resolved
 sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
 sudo systemctl mask dnsmasq.service
 
-
 sudo sh -c "echo 'ap_name: hans' >> /etc/comitup.conf"
 sudo sh -c "echo 'ap_password: hhhhhhhh' >> /etc/comitup.conf"
 
+
 sudo raspi-config nonint do_hostname hans
-sudo cp /home/pi/hans_install/config.txt /boot/config.txt
+sudo cp /home/pi/hans_install/files/config.txt /boot/config.txt
 
 #RUST
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
