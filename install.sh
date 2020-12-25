72a5fd9a5cec93695ccbd6c2d74854ae4b987fd9
Matthias Budde <matt@nordseele.com>
2020-12-25 20:06:25 +0100

    ...

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
+#!/bin/sh
+
 sudo apt update -y
 
 sudo SKIP_WARNING=1 rpi-update
 
-sudo cp /home/pi/hans_install/files/hans.service /etc/systemd/system
+PACKAGES="libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev libasound2-dev git"
 
-sudo systemctl enable hans
-
-sudo apt-get install libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev libasound2-dev -y
+sudo apt-get install $PACKAGES -y
 
 #MIDI 
 cd && git clone https://github.com/nordseele/ttymidi.git
@@ -28,24 +28,11 @@ sudo cp /home/pi/hans_install/files/cmdline.txt /boot
 
 #I2C
 sudo raspi-config nonint do_i2c 0
-sudo sh -c "echo 'dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=2,i2c_gpio_sda=5,i2c_gpio_scl=6' >> /boot/config.txt"
-
+sudo cp /home/pi/hans_install/files/config.txt /boot/config.txt
 
 sudo raspi-config nonint do_expand_rootfs
 
-
-# NETWORK ? :-/
-#sudo apt-get install comitup -y
-#sudo systemctl disable systemd-resolved
-#sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
-#sudo systemctl mask dnsmasq.service
-
-#sudo sh -c "echo 'ap_name: hans' >> /etc/comitup.conf"
-#sudo sh -c "echo 'ap_password: hhhhhhhh' >> /etc/comitup.conf"
-
-
 sudo raspi-config nonint do_hostname hans
-sudo cp /home/pi/hans_install/files/config.txt /boot/config.txt
 
 #RUST
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
