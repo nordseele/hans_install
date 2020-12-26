d4b123214861357cda683de82d596134a2e6f5de
Matthias Budde <matt@nordseele.com>
2020-12-26 14:40:42 +0100

    edit install script

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ sudo apt-get install $PACKAGES -y
 cd && git clone https://github.com/nordseele/hans_install.git 
 
 #MIDI 
-cd && git clone https://github.com/moddevices/mod-ttymidi.git
-cd mod-ttymidi
+cd && git clone https://github.com/nordseele/ttymidi.git
+cd ttymidi
 make
 sudo make install
 
@@ -23,8 +23,6 @@ sudo systemctl enable amidiauto
 
 sudo cp /home/pi/hans_install/files/mod-ttymidi.service /etc/systemd/system/mod-ttymidi.service
 sudo systemctl enable mod-ttymidi.service
-sudo cp /home/pi/hans_install/files/cmdline.txt /boot
-
 
 #I2C
 sudo raspi-config nonint do_i2c 0
@@ -33,6 +31,7 @@ sudo cp /home/pi/hans_install/files/config.txt /boot/config.txt
 sudo raspi-config nonint do_hostname hans
 sudo raspi-config nonint do_expand_rootfs
 
+sudo sed -i 's/console=serial0,115200//' cmdline.txt
 
 #RUST
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
